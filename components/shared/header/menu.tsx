915ba62723671001a62082edc911c5afb1279054
aref <arefsalarieh@gmail.com>
2025-01-26 23:33:36 +0400

    layout finish

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,21 @@
+import { ShoppingCartIcon, UserIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export default function Menu() {
+  return (
+    <div className='flex justify-end'>
+      <nav className='flex gap-3 w-full'>
+        <Link href='/signin' className='header-button flex items-center'>
+          Hello , Sign in
+        </Link>
+
+        <Link href='/cart' className='header-button'>
+          <div className="felx item-end">
+             <ShoppingCartIcon className='h-8 w-8' />
+             Cart
+          </div>
+        </Link>
+      </nav>
+    </div>
+  )
+}
